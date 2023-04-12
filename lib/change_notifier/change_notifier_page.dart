9980eba8948314901ca12e388b3711200f73e6b0
dionatanlaranjeira <dionatanribeiro.netrr@gmail.com>
2023-04-12 08:51:07 -0400

    Create change_notifier_page.dart

---
+++ lib/change_notifier/change_notifier_page.dart
@@ -0,0 +1,123 @@
+import 'dart:math';
+
+import 'package:calculadora_imc_default_state_manager/widgets/imc_gauge.dart';
+import 'package:calculadora_imc_default_state_manager/widgets/imc_gauge_range.dart';
+import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+class ChangeNotifierPage extends StatefulWidget {
+  const ChangeNotifierPage({Key? key}) : super(key: key);
+
+  @override
+  State<ChangeNotifierPage> createState() => _ChangeNotifierPageState();
+}
+
+class _ChangeNotifierPageState extends State<ChangeNotifierPage> {
+  final formKey = GlobalKey<FormState>();
+  final pesoEC = TextEditingController();
+  final alturaEC = TextEditingController();
+  var imc = 0.0;
+
+  Future<void> _calcularIMC(
+      {required double peso, required double altura}) async {
+    setState(() {
+      imc = 0;
+    });
+
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      imc = peso / pow(altura, 2);
+    });
+  }
+
+  @override
+  void dispose() {
+    pesoEC.dispose();
+    alturaEC.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ChangeNotifier'),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                ImcGauge(imc: imc),
+                Text('Seu IMC: ${imc.toStringAsFixed(2)}'),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: pesoEC,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: 'Peso'),
+                  inputFormatters: [
+                    CurrencyTextInputFormatter(
+                      locale: 'pt_BR',
+                      enableNegative: false,
+                      symbol: '',
+                      decimalDigits: 1,
+                      turnOffGrouping: true,
+                    ),
+                  ],
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Peso obrigatório';
+                    }
+                  },
+                ),
+                TextFormField(
+                  controller: alturaEC,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: 'Altura'),
+                  inputFormatters: [
+                    CurrencyTextInputFormatter(
+                      locale: 'pt_BR',
+                      enableNegative: false,
+                      symbol: '',
+                      decimalDigits: 2,
+                      turnOffGrouping: true,
+                    ),
+                  ],
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Altura obrigatória';
+                    }
+                  },
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      var formValid = formKey.currentState?.validate() ?? false;
+
+                      if (formValid) {
+                        var formatter = NumberFormat.simpleCurrency(
+                          locale: 'pt_BR',
+                          decimalDigits: 2,
+                        );
+                        double peso = formatter.parse(pesoEC.text) as double;
+                        double altura =
+                            formatter.parse(alturaEC.text) as double;
+
+                        _calcularIMC(peso: peso, altura: altura);
+                      }
+                    },
+                    child: const Text('Calcular IMC')),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
